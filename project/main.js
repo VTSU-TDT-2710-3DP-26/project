5326b9613d18658d722cdfedae3eadb52868dacf
Wyman, Taiga J. <tjw00435@castleton.edu>
2026-07-07 14:23:39 -0400

    maze p1

--- project/main.js
+++ project/main.js
@@ -3,18 +3,22 @@ import { drawAxes } from '@/utils/axes.js';
 import { vector } from "@/utils/vec3.js";
 
 // Variables 
-const length = 6
-const width = 6
-let Movement = vector(0, -0.2, 0);
+const length = 7
+const width = 7
+let position = vector(0, -0.2, 0);
+let velocity = vector(0, 0, 0);
+let gravity = vector(0, 9.8, 0);
 let grid = [
-    [2, 1, 1, 1, 1, 3],
-    [0, 1, 6, 2, 5, 3],
-    [2, 0, 0, 0, 1, 3],
-    [0, 5, 2, 1, 0, 3],
-    [0, 1, 5, 0, 0, 3],
-    [4, 4, 4, 4, 4, 7],
+    [2, 1, 1, 1, 1, 1, 3],
+    [0, 1, 1, 2, 5, 0, 3],
+    [2, 0, 0, 0, 1, 0, 3],
+    [0, 5, 2, 1, 0, 0, 3],
+    [0, 1, 5, 0, 2, 5, 3],
+    [0, 1, 1, 0, 1, 1, 3],
+    [4, 4, 4, 4, 4, 4, 7],
 
 ];
+
 
 export function preload() {
 
@@ -22,6 +26,7 @@ export function preload() {
 
 export function setup() {
     camera(300, -200, 700);
+
 }
 
 export function draw(t, dt) {
@@ -36,9 +41,14 @@ export function draw(t, dt) {
 
     stroke(0);
     strokeWeight(1);
-    movement();
+    movePlayer2(t, dt);
+    drawPlayer();
     //  floor();
     wall();
+    push();
+    translate(455, -10, 475);
+    end();
+    pop();
 
 
 }
@@ -105,8 +115,15 @@ function wall() {
                 fill('black');
                 box(100, 5, 100);
             }
-            
+            if (grid[z][x] == 6) {
+                fill('black');
+                box(100, 5, 100);
+                translate(-50, -25, 0)
+                rotateY(90);
+                fill('grey');
+                box(100, 50, 5)
             }
+
             pop();
         }
 
@@ -116,7 +133,11 @@ function wall() {
 
 }
 
+function hitbox() {
 
+
+
+}
 
 function oldwall() {
     for (let p = 0; p < length; p++) {
@@ -148,42 +169,208 @@ function oldwall() {
         pop();
     }
 }
-function movement() {
-    let x = 0;
-    let y = -.2;
-    let z = 0;
-    {
 
-        if (keyIsDown(LEFT_ARROW)) {
-            Movement.x -= 5;
-        }
-        if (keyIsDown(RIGHT_ARROW)) {
-            Movement.x += 5;
-        }
-        if (keyIsDown(UP_ARROW)) {
-            Movement.z -= 5;
-        }
-        if (keyIsDown(DOWN_ARROW)) {
-            Movement.z += 5;
-        }
-        push();
-        fill(255, 64, 255);
-        translate(Movement.x, y, Movement.z);
-        push();
-        translate(0, -60, 0);
-        rotateZ(180);
-        fill('green');
-        cone(20, 15);
-        pop();
-        translate(0, -45, 0);
-        fill(225, 220, 177);
-        sphere(15);
-        translate(0, 20, 0);
-        cylinder(10, 50);
-        pop();
+function movePlayer() {
 
-        pop();
-        scale(1.5, 1.5, 1.5);
-
+    if (keyIsDown(LEFT_ARROW)) {
+        position.x -= 5;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        position.x += 5;
+    }
+    if (keyIsDown(UP_ARROW)) {
+        position.z -= 5;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        position.z += 5;
     }
 }
+function movePlayer2(t, dt) {
+    if (keyIsDown(LEFT_ARROW)) {
+        velocity.x -= 5;
+    }
+
+    //If right arrow is pressed then the velocity goes up to the right or possitive x 
+    if (keyIsDown(RIGHT_ARROW)) {
+        velocity.x += 5;
+    }
+
+    // if the up arrrow is pressed then the z velocity goes down
+    if (keyIsDown(UP_ARROW)) {
+        velocity.z -= 5;
+    }
+
+    // z velocity goes up when down arrow is pressed
+    if (keyIsDown(DOWN_ARROW)) {
+        velocity.z += 5;
+    }
+
+    // if space bar is pressed and y position is zero then you can jump 
+    if (keyIsDown(32) && position.y == 0) {
+        velocity.y = 5;
+    }
+
+    //calculate position and stuff
+    position = position.plus(velocity.times(dt));
+    velocity = velocity.plus(gravity.times(dt));
+
+    //if  your on the ground then your speed of movement is velocity x time 0.95
+    if (position.y == 0) {
+        velocity.x = velocity.x * .95;
+        velocity.z = velocity.z * .95;
+    }
+
+    // if position y is greater than zero then your y movement is stalled and you cant gain more than what the jump gives you as y velocity 
+    if (position.y > 0) {
+        position.y = 0;
+        velocity.y = 0;
+    }
+
+    //moves and creates the sphere
+    push();
+    fill(0, 0, 0);
+    scale(1, 0, 1);
+    translate(position.x, 0, position.z);
+    sphere(.3);
+    pop();
+
+    // starting the position for the floating part
+    push();
+    fill(255, 64, 64);
+    translate(0, -.4, 0);
+    if (position.y == 0) {
+        //makes the ball move up and down
+        translate(0, - Math.sin(3 * t) * .05, 0);
+    }
+    translate(position.x, position.y, position.z);
+    sphere(.3);
+    pop();
+
+
+
+
+    console.log("position", position.x, position.z);
+
+    let gridX = Math.floor((position.x + 75) / 150);
+    let gridZ = Math.floor((position.z + 75) / 150);
+
+    let localX = position.x - gridX * 150;
+    let localZ = position.z - gridZ * 150;
+
+    console.log("grid", gridX, gridZ);
+
+    let northWall = grid[gridZ][gridX] == 1 || grid[gridZ][gridX] == 2;
+    let southWall = grid[gridZ + 1][gridX] == 1 || grid[gridZ + 1][gridX] == 2;
+    let southWallblank = grid[gridZ + 1][gridX] == 4 || grid[gridZ + 1][gridX] == 2;
+
+    let westWall = grid[gridZ][gridX] == 0 || grid[gridZ][gridX] == 2;
+    let eastWall = grid[gridZ][gridX + 1] == 0 || grid[gridZ][gridX + 1] == 2;
+    let eastWallblank = grid[gridZ][gridX + 1] == 3 || grid[gridZ][gridX + 1] == 2;
+    let westWallblank = grid[gridZ][gridX] == 4 || grid[gridZ][gridX] == 2;
+
+
+    console.log("local", localX, localZ, "north", northWall, "south", southWall, "east", eastWall, "west", westWall);
+
+    if (northWall == true) {
+        //Z-
+        if (localZ < -60) {
+            position.z = position.z + 5;
+            velocity = vector(0, 0, 0);
+        }
+    }
+    if (southWall == true) {
+        //Z+
+        if (localZ > 60) {
+            position.z = position.z - 5;
+            velocity = vector(0, 0, 0);
+
+        }
+
+    }
+    if (southWallblank == true) {
+        //Z+
+        if (localZ > 60) {
+            position.z = position.z - 5;
+            velocity = vector(0, 0, 0);
+
+        }
+
+    }
+    if (eastWall == true) {
+        //Z+
+        if (localX > 60) {
+            position.x = position.x - 5;
+            velocity = vector(0, 0, 0);
+
+        }
+
+    }
+    if (westWall == true) {
+        //Z+
+        if (localX < -60) {
+            position.x = position.x + 5;
+            velocity = vector(0, 0, 0);
+
+        }
+
+    }
+    if (eastWallblank == true) {
+        //Z+
+        if (localX > 60) {
+            position.x = position.x - 5;
+            velocity = vector(0, 0, 0);
+
+        }
+
+    }
+    if (westWallblank == true) {
+        //Z+
+        if (localX < -60) {
+            position.x = position.x + 5;
+            velocity = vector(0, 0, 0);
+
+        }
+    }
+}
+function drawPlayer() {
+    push();
+    fill('yellow');
+    translate(position.x, position.y, position.z);
+    push();
+    translate(0, -60, 0);
+    rotateZ(180);
+    fill('green');
+    cone(20, 15);
+    pop();
+    translate(0, -45, 0);
+    fill(225, 220, 177);
+    sphere(15);
+    translate(0, 20, 0);
+    cylinder(10, 50);
+    pop();
+    scale(1.5, 1.5, 1.5);
+
+}
+
+function end() {
+    scale(0.4);
+    translate(0, 0, 0)
+    strokeWeight(4);
+    stroke('white');
+    line(0, 0, 0, 0, 0, 100);
+    line(50, 0, 0, 0, 0, 0);
+    line(50, 0, 50, 0, 0, 50);
+    line(50, 0, 100, 0, 0, 100);
+
+    line(75, 0, 0, 75, 0, 100);
+    line(75, 0, 0, 125, 0, 100);
+    line(125, 0, 100, 125, 0, 0);
+
+    line(150, 0, 0, 150, 0, 100);
+    line(150, 0, 0, 185, 0, 25);
+    line(150, 0, 100, 185, 0, 75);
+    line(185, 0, 25, 185, 0, 75);
+
+
+}
+
